b04317b9e00e62a3b4a352d62d45455e53c0a144
Allen Lewis <allenlewis32@gmail.com>
2023-04-18 00:25:06 +0530

    Login implemented

--- AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -17,32 +17,35 @@ namespace AppointmentScheduler.Controllers
 		// POST: Account/Register
 		public IActionResult Register(RegisterModel model)
 		{
-			try
+			if (ModelState.IsValid)
 			{
-				// check if email is already registered
-				SqlCommand cmd = new($"select count(*) from users where email='{model.Email}'", Connection.conn);
-				int? res = (int?)cmd.ExecuteScalar();
-				if (res != null && res > 0) // email already registered
+				try
 				{
-					ModelState.AddModelError("", "Email already registered");
+					// check if email is already registered
+					SqlCommand cmd = new($"select count(*) from users where email='{model.Email}'", Connection.conn);
+					int? res = (int?)cmd.ExecuteScalar();
+					if (res != null && res > 0) // email already registered
+					{
+						ModelState.AddModelError("", "Email already registered");
+					}
+					else
+					{
+						cmd = new("createUser", Connection.conn);
+						cmd.CommandType = System.Data.CommandType.StoredProcedure;
+						cmd.Parameters.AddWithValue("@email", model.Email);
+						cmd.Parameters.AddWithValue("@password", model.Password);
+						cmd.Parameters.AddWithValue("@phone", model.Phone);
+						cmd.Parameters.AddWithValue("@name", model.Name);
+						cmd.ExecuteNonQuery();
+						HttpContext.Session.SetString("email", model.Email);
+						HttpContext.Session.SetString("name", model.Name);
+						return RedirectToAction("Index", "Home");
+					}
 				}
-				else
+				catch
 				{
-					cmd = new("createUser", Connection.conn);
-					cmd.CommandType = System.Data.CommandType.StoredProcedure;
-					cmd.Parameters.AddWithValue("@email", model.Email);
-					cmd.Parameters.AddWithValue("@password", model.Password);
-					cmd.Parameters.AddWithValue("@phone", model.Phone);
-					cmd.Parameters.AddWithValue("@name", model.Name);
-					cmd.ExecuteNonQuery();
-					HttpContext.Session.SetString("email", model.Email);
-					HttpContext.Session.SetString("name", model.Name);
-					return RedirectToAction("Index", "Home");
+					ModelState.AddModelError("", "Unable to register user");
 				}
-			}
-			catch
-			{
-				ModelState.AddModelError("", "Unable to register user");
 			}
 			return View(model);
 		}
@@ -52,6 +55,45 @@ namespace AppointmentScheduler.Controllers
 		{
 			HttpContext.Session.Clear();
 			return RedirectToAction("Index", "Home");
+		}
+
+		// GET: Accounr/Login
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		// POST: Account/Login
+		public IActionResult Login(LoginModel model)
+		{
+			if(ModelState.IsValid)
+			{
+				try
+				{
+					SqlCommand cmd = new("checkLoginAndGetName", Connection.conn);
+					cmd.CommandType = System.Data.CommandType.StoredProcedure;
+					cmd.Parameters.AddWithValue("@email", model.Email);
+					cmd.Parameters.AddWithValue("@password", model.Password);
+					string? res = (string?)cmd.ExecuteScalar();
+					if(res != null)
+					{
+						HttpContext.Session.SetString("email", model.Email);
+						HttpContext.Session.SetString("name", res!);
+						return RedirectToAction("Index", "Home");
+					}
+					else
+					{
+						ModelState.AddModelError("", "Invalid email/password");
+					}
+				}
+				catch
+				{
+					ModelState.AddModelError("", "Unable to login");
+				}
+			}
+			return View(model);
 		}
 	}
 }
